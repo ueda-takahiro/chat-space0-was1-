7b8672d483844a89137081c927db5d9a00238372
ueda-takahiro <ueda196@gmail.com>
2019-07-26 15:12:24 +0900

    aws rails

--- chat-space1/app/assets/javascripts/message.js
+++ chat-space1/app/assets/javascripts/message.js
@@ -53,30 +53,32 @@ $(document).on('turbolinks:load', function(){
     })
    })
 
-  var reloadMessages = function() {
+  
+   var reloadMessages = function(){
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-    var last_message_id = $('.messages__message').data("message-id"); 
-   
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data:{ last_id: last_message_id }
-    })
-    .done(function (messages) { 
-      var insertHTML = '';
-      messages.forEach(function (message) {
-        insertHTML = buildHTML(message); 
-        $('.messages').append(insertHTML);
+      var last_message_id = $('.messages__message').last().data("id");
+
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {last_id: last_message_id}
       })
-      
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-   
-  })
-    .fail(function () {
-      alert('自動更新に失敗しました');
-    })
-  }
-} ;
-setInterval(reloadMessages, 5000);
-})
+
+      .done(function(messages){
+        var insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML += buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+
+      .fail(function(){
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+});
